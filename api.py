47d917bc9c3997096718b3eb8a9d3ceb4b9363e7
Chaitanya360 <chaitanyajadhav360@gmail.com>
2022-05-14 23:37:08 +0530

    few chages to map register in front-ent

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from urllib import response
+from xml.dom import UserDataHandler
 from flask import Flask, render_template, request, jsonify, url_for, redirect, flash
 import os
 import datetime
@@ -41,6 +42,8 @@ def login():
 def view(userName):
     db = DatabaseConnection("identityDB")
     docs = db.fetchAlldocuments(userName)
+    # for doc in docs:
+    # print(doc)
     docsSeparated = {}
     for doc in docs:
         print(doc["documentName"])
@@ -49,7 +52,7 @@ def view(userName):
         else:
             docsSeparated[doc["documentName"]].append(doc)
 
-    # print(docsSeparated)
+    print(docsSeparated)
     # return render_template('view.html', userName=userName, docsSeparated=docsSeparated)
     # docsSeparated = {'test': [{'_id': ObjectId('62709529f3b89ef4bfca24bb'), 'userName': 'nickghule', 'documentName': 'test', 'timestamp': datetime.datetime(2022, 5, 3, 8, 6, 25, 759000), 'documentData': {'no': '121212', 'hiii': '55555'}}, {'_id': ObjectId('627cc31dc35ad85d4a9c80a6'), 'userName': 'nickghule', 'documentName': 'test', 'timestamp': datetime.datetime(2022, 5, 13, 8, 6, 25, 759000), 'documentData': {'no': '121212', 'hiii': '55555'}}], 'doc 2': [
     #     {'_id': ObjectId('627cc332c35ad85d4a9c80a7'), 'userName': 'nickghule', 'documentName': 'doc 2', 'timestamp': datetime.datetime(2022, 5, 13, 8, 6, 25, 759000), 'documentData': {'no': '121212', 'hiii': '55555'}}], 'Doc 3': [{'_id': ObjectId('627cd4f30d8e9aaa07c60705'), 'userName': 'nickghule', 'documentName': 'Doc 3', 'timestamp': datetime.datetime(2022, 5, 3, 8, 6, 25, 759000), 'documentData': {'Attr1': 'Val 1', 'Attr2': 'Val 2'}}]}
@@ -76,7 +79,8 @@ def share(shareFrom, shareTo, documentId):
     if result:
         response = jsonify(json.loads(dumps(({'status': True}))))
     else:
-        response = jsonify(json.loads(dumps(({'status': False}))))
+        response = jsonify(json.loads(
+            dumps(({'status': False, 'msg': "you messed up something"}))))
 
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
@@ -118,22 +122,18 @@ def check_history(userName, shareFrom, documentName):
     return "Not shared"
 
 
-@app.route('/verifier/<string:userName>/<string:shareFrom>/<string:documentName>/verify', methods=['GET', 'POST'])
-def verify(userName, shareFrom, documentName):
+@app.route('/verifier/<string:shareFrom>/<string:documentName>/verify', methods=['GET', 'POST'])
+def verify(shareFrom, documentName):
     db = DatabaseConnection("identityDB")
     bc = BlockchainConnection()
-
     hist = bc.getDocumentHistory(shareFrom, documentName)
-
     db = DatabaseConnection("identityDB")
     doc = list(db.fetchDocument(shareFrom, documentName))[-1]
     # bc.addDocument("nickghule", "test", hashlib.sha256(dumps(doc).encode()).hexdigest())
-
-    if hashlib.sha256(dumps(doc).encode()).hexdigest() ==hist[-1]:
+    if hashlib.sha256(dumps(doc).encode()).hexdigest() == hist[-1]:
         response = jsonify(json.loads(dumps(({'varified': True}))))
     else:
         response = jsonify(json.loads(dumps(({'varified': False}))))
-
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
@@ -144,12 +144,10 @@ def issue(doc):
     userName = data.get("username")
     documentName = data.get("documentName")
     rawdocumentData = data.get("values")
-
     db = DatabaseConnection("identityDB")
     bc = BlockchainConnection()
-    
-    #TODO : check if document already exists
-    #TODO : check if user exists
+    # TODO : check if document already exists
+    # TODO : check if user exists
     doc = json.loads(doc)
     if not len(rawdocumentData):
         response = jsonify(json.loads(dumps(({'issued': False}))))
@@ -160,21 +158,16 @@ def issue(doc):
     documentData = {}
     for doc in rawdocumentData:
         documentData[doc.get("key")] = doc.get("value")
-
     db.addDocument(userName, documentName, documentData)
 
     doc = list(db.fetchDocument(userName, documentName))[-1]
-    bc.addDocument(userName, documentName, hashlib.sha256(dumps(doc).encode()).hexdigest())
-
+    bc.addDocument(userName, documentName, hashlib.sha256(
+        dumps(doc).encode()).hexdigest())
 
     response = jsonify(json.loads(dumps(({'issued': True}))))
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-    
-    
-    
-    
-    print(json.loads(doc))
+
 
 @app.route('/login/<string:userName>/<string:passwd>/', methods=['GET', 'POST'])
 def login_user(userName, passwd):
@@ -186,7 +179,8 @@ def login_user(userName, passwd):
         return response
     else:
         if userDetail.get("password") == passwd:
-            response = jsonify(json.loads(dumps(({'status': True}))))
+            response = jsonify(json.loads(
+                dumps(({'status': True, 'userType': userDetail.get('userType')}))))
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
         else:
@@ -194,20 +188,23 @@ def login_user(userName, passwd):
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
 
+
 @app.route('/register/<string:userName>/<string:passwd>/<int:type>/<string:name>/', methods=['GET', 'POST'])
 def register_user(userName, passwd, type, name):
     db = DatabaseConnection("identityDB")
     userDetail = db.getUser(userName)
     if userDetail:
-        response = jsonify(json.loads(dumps(({'status': False}))))
+        response = jsonify(json.loads(
+            dumps(({'status': False, "msg": "username is taken"}))))
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
     else:
-        
-        db.addUser(userName, passwd, type, name)
+
+        db.addUser(userName, name, passwd, type)
         response = jsonify(json.loads(dumps(({'status': True}))))
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
